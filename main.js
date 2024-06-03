d1e2b7eb23b65e5193460c1fd4140888fb7255f8
wanyos <juanjoromero9@gmail.com>
2024-06-03 12:59:13 +0200

    chnage in file main

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 
 const signupForm = document.querySelector('#signup-form');
+
+signupForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const signupEmail = document.querySelector('#signup-email');
+    const signupPassword = document.querySelector('#signup-password');
+
+    console.log('submit add');
+});
